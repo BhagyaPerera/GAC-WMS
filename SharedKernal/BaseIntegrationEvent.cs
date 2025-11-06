157ff298080f9a89f241cd03b3e46e9728aacfda
Bhagya <wbmadushani@gmail.com>
2025-11-07 01:13:29 +1100

    Add shared kernal Project

---
+++ SharedKernal/BaseIntegrationEvent.cs
@@ -0,0 +1,28 @@
+﻿using SharedKernal.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedKernal
+{
+    public class BaseIntegrationEvent : IApplicationEvent
+    {
+        public DateTimeOffset DateOccurred { get; protected set; } = DateTimeOffset.UtcNow;
+        public string EventType { set; get; }
+
+        public BaseIntegrationEvent(string eventName)
+        {
+            EventType = eventName;
+        }
+
+        public BaseIntegrationEvent()
+        { } // Json Serialization Need this
+
+        public string GetIntegrationName()
+        {
+            return EventType;
+        }
+    }
+}
---
+++ SharedKernal/Interfaces/IApplicationEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedKernal.Interfaces
+{
+    public interface IApplicationEventHandler<T> where T : IApplicationEvent
+    {
+        public Task<bool> Handle(T applicationEvent);
+    }
+}
